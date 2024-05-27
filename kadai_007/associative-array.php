fac498bab1c1f4785f4a18642d4214a1ecf2f54a
masamiru <162574177+masamiru@users.noreply.github.com>
2024-05-27 20:27:37 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <body>
+
+     <p>
+         <?php
+         $data = ['name'=>'onion','price'=>200,'weight'=>160,];
+
+         print_r($data);
+
+         ?>
+     </p>
+ </body>
+ 
+ </html>
